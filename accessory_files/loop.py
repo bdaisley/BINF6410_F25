65266e8c320cb2b72cbf73e75825586125c3fde7
bdaisley <bdaisley@uwo.ca>
2025-11-11 00:31:58 -0500

    no message

--- accessory_files/loop.py
+++ accessory_files/loop.py
@@ -4,5 +4,5 @@ print (sys.argv[0]) #this is the first element of the arguments.
 sys.argv[0]
 file= open(sys.argv[1]) #sys.argv[1] contains the 2nd argument
 for i in file:
-    print (i.rstrip)
+    print(i.rstrip())
 print ("goodbye")
